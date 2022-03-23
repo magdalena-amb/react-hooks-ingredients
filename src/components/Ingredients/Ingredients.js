a6b082e42be164c3eff81d070258572ae0899137
Magdalena Ambrozy <magda.ambrozy@gmail.com>
2022-03-23 15:22:08 +0100

    Add useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,39 @@
-import React, { useState, useCallback } from "react";
+import React, { useEffect, useReducer, useState, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
-function Ingredients() {
-  const [userIngredients, setUserIngredients] = useState([]);
+const ingredientsReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Error in ingredientsReducer");
+  }
+};
+
+const Ingredients = () => {
+  const [userIngredients, dispatchIngredients] = useReducer(
+    ingredientsReducer,
+    []
+  );
+
+  //const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS", userIngredients);
+  }, [userIngredients]);
+
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setUserIngredients(filteredIngredients);
+    dispatchIngredients({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
   function submitHandler(ingr) {
@@ -30,10 +52,10 @@ function Ingredients() {
         return res.json();
       })
       .then((resData) => {
-        setUserIngredients((prevIngr) => [
-          ...prevIngr,
-          { ...ingr, id: resData.name },
-        ]);
+        dispatchIngredients({
+          type: "ADD",
+          ingredient: { id: resData.name, ...ingr },
+        });
       });
   }
 
@@ -44,20 +66,17 @@ function Ingredients() {
       {
         method: "DELETE",
       }
-    )
-      .then((res) => {
+    ).then(res => {
         setIsLoading(false);
-        setUserIngredients((prevIngr) => {
-          prevIngr.filter((ingr) => ingr.id !== ingredientId);
-        });
+        dispatchIngredients({type: 'DELETE', id : ingredientId})
+    })
         // console.log(userIngredients);
-      })
       .catch((error) => {
         setError(error.message);
       });
   }
 
-  function clearError() {
+  function clearError () {
     setError(null);
     setIsLoading(false);
   }
@@ -76,6 +95,6 @@ function Ingredients() {
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
